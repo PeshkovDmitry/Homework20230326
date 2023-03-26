ec4a144cb299796e2e67f3b9240c7628c052e059
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-26 15:39:13 +0300

    Домашнее задание

---
+++ Task50/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// позиции элемента в двухмерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix, int[] position)
+{
+    if (position[0] >= matrix.GetLength(0) || position[1] >= matrix.GetLength(1))
+        Console.WriteLine($"В матрице нет элемента с позицией [{position[0]},{position[1]}]");
+    else 
+        Console.WriteLine($"-> {matrix[position[0],position[1]]}");
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.Write("Введите позицию искомого элемента: ");
+int[] position = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+ReleaseMatrix(matrix, position);
---
+++ Task52/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 52. Задайте двухмерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++) average += matrix[i,j];
+        average /= matrix.GetLength(0);
+        Console.WriteLine($"Для столбца {j} среднее арифметическе {Math.Round(average, 2)}");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
